c20141a9fb946a1ef3b257d5955470c7ef5f878a
jeongha-joo <jha970107@gmail.com>
2021-03-24 15:39:08 +0900

    https

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,7 @@ const COORDS = "coords";
 
 function getWeather(lat, lng) {
   fetch(
-    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&id=524901&appid=${API_KEY}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&id=524901&appid=${API_KEY}&units=metric`
   )
     .then(function (response) {
       return response.json();
